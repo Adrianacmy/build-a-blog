1f6848046da4efa4ca256a80a5018c5eee66c86d
Adrianacmy <adrianamullaney@gmail.com>
2017-07-12 21:46:43 -0700

    change main to app

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, request, redirect, render_template, session, flash, url_for
+from flask import Flask, request, redirect, render_template, session, flash, url_for, make_response
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+import datetime
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -26,22 +26,20 @@ class Post(db.Model):
 
 @app.route('/')
 def index():
-    return redirect('/blog')
+    count = int(request.cookies.get('visit_count', 0))
+    count += 1
+
+    # resp = make_response(message)
+    # resp.set_cookie('visit-count', str(count))
+    # return resp
+
+    resp = make_response(redirect('/blog'))
+    resp.headers['visit_count'] = str(count)
+    return resp
 
 
-@app.route('/blog', methods=['POST', 'GET'])
+@app.route('/blog', methods=['GET'])
 def display_blog():
-    # if request.method == 'POST':
-    #     title = request.form['title']
-    #     body = request.form['body']
-    #     if title and body:
-    #         newpost = Post(title, body)
-    #         db.session.add(newpost)
-    #         db.session.commit()
-    #     else:
-    #         flash("Title and body can not be empty.")
-    #         return render_template("newpost.html", title=title, body=body)
-
     posts = Post.query.all()
     return render_template('posts.html', posts=posts)
 
@@ -51,25 +49,27 @@ def newpost():
     if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
+        pub_date = request.form['pub_date']
+
         if title and body:
             newpost = Post(title, body)
             db.session.add(newpost)
             db.session.commit()
-            return redirect('/post?id='+str(newpost.id))
+            return redirect('/single_post?id='+str(newpost.id))
         else:
             flash("Title and body can not be empty.", "warning")
-            return render_template("newpost.html", title=title, body=body)
+            return render_template("newpost.html", title=title, body=body, pub_date=pub_date)
 
     else:
         return render_template("newpost.html")
 
 
-@app.route('/post', methods=['GET'])
+@app.route('/single_post', methods=['GET'])
 def single_post():
     id = request.args.get('id')
-    post = Post.query.filter_by(id=id).first()
+    single_post = Post.query.filter_by(id=id).first()
 
-    return render_template('post.html', post=post)
+    return render_template('single_post.html', post=single_post)
 
 
 if __name__ == "__main__":
